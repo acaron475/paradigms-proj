cbfd663b6f0c790eaa2ad32fca10f296f1dcc242
acaron475 <acaron1223@gmail.com>
2017-05-05 22:02:19 -0400

    couple of image fixes and created reset function to place balls in starting loccations

--- billiards.py
+++ billiards.py
@@ -4,10 +4,9 @@ from pygame.locals import *
 from sprites import *
 import os
 
-
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((1138,643))
+    screen = pygame.display.set_mode((TABLE_WIDTH,TABLE_HEIGHT))
     pygame.display.set_caption('Billiards')
     pygame.mouse.set_visible(0)
     
@@ -19,11 +18,9 @@ if __name__ == '__main__':
     pygame.display.flip()
     
     clock = pygame.time.Clock()
-    table = Table('table.png')
-    cueball = Ball('ball_16.png')
-    ball = Ball('ball_1.png')
-    cueball.set_position(3*table.rect.width/4-27, table.rect.height/2)
-    ball.set_position(table.rect.width/4, table.rect.height/2)
+    table = Table()
+    balls = Balls()
+    balls.reset()
     
     going = True
     while going:
@@ -32,11 +29,12 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == QUIT:
                 going = False
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                going = False
         
         screen.blit(background,(0,0))
         table.draw(screen)
-        cueball.draw(screen)
-        ball.draw(screen)
+        balls.draw(screen)
         pygame.display.flip()
     
     pygame.quit()
--- sprites.py
+++ sprites.py
@@ -4,6 +4,10 @@ from pygame.locals import *
 import math
 import os
 
+TABLE_HEIGHT = 643
+TABLE_WIDTH = 1138
+BALL_RADIUS = 35
+
 def load_image(name,colorkey=None):
     fullname = os.path.join('data/images',name)
     try:
@@ -18,10 +22,40 @@ def load_image(name,colorkey=None):
         image.set_colorkey(colorkey,RLEACCEL)
     return image,image.get_rect()
 
+class Balls():
+    def __init__(self):
+        self.balls = [Ball('ball_16.png',0)]
+        for i in range(1,16):
+            ball = Ball('ball_' + str(i) + '.png',i)
+            self.balls.append(ball)
+    
+    def reset(self):
+        self.balls[0].set_position(3*TABLE_WIDTH/4-27, TABLE_HEIGHT/2)
+        self.balls[1].set_position(TABLE_WIDTH/4+27, TABLE_HEIGHT/2)
+        self.balls[9].set_position(self.balls[1].rect.x-33,self.balls[1].rect.y-18)
+        self.balls[3].set_position(self.balls[1].rect.x-33,self.balls[1].rect.y+18)
+        self.balls[4].set_position(self.balls[9].rect.x-33,self.balls[9].rect.y-18)
+        self.balls[8].set_position(self.balls[9].rect.x-33,self.balls[9].rect.y+18)
+        self.balls[10].set_position(self.balls[3].rect.x-33,self.balls[3].rect.y+18)
+        self.balls[11].set_position(self.balls[4].rect.x-33,self.balls[4].rect.y-18)
+        self.balls[5].set_position(self.balls[4].rect.x-33,self.balls[4].rect.y+18)
+        self.balls[13].set_position(self.balls[8].rect.x-33,self.balls[8].rect.y+18)
+        self.balls[6].set_position(self.balls[10].rect.x-33,self.balls[10].rect.y+18)
+        self.balls[7].set_position(self.balls[11].rect.x-33,self.balls[11].rect.y-18)
+        self.balls[12].set_position(self.balls[11].rect.x-33,self.balls[11].rect.y+18)
+        self.balls[2].set_position(self.balls[5].rect.x-33,self.balls[5].rect.y+18)
+        self.balls[14].set_position(self.balls[13].rect.x-33,self.balls[13].rect.y+18)
+        self.balls[15].set_position(self.balls[6].rect.x-33,self.balls[6].rect.y+18)
+    
+    def draw(self,surface):
+        for ball in self.balls:
+            ball.draw(surface)        
+
 class Ball(sprite.Sprite):
-    def __init__(self,image):
+    def __init__(self,image,num):
         sprite.Sprite.__init__(self)
         self.image,self.rect = load_image(image,-1)
+        self.num = num
         self.speed = 0
         self.angle = 0
         
@@ -33,9 +67,9 @@ class Ball(sprite.Sprite):
         surface.blit(self.image,self.rect)
         
 class Table(sprite.Sprite):
-    def __init__(self,image):
+    def __init__(self):
         sprite.Sprite.__init__(self)
-        self.image,self.rect = load_image(image)
+        self.image,self.rect = load_image('table.png')
 
     def draw(self,surface):
         surface.blit(self.image,self.rect)
